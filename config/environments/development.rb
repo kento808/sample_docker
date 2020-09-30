5429a6ee147c347db15471d61fa2052b5ca7bb94
kento higa <kento_higa_0401@yahoo.co.jp>
2020-09-30 14:41:03 +0900

    edit mail setting for password reset

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,14 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => ENV['GMAIL_ADDRESS'],
+    :password => ENV['GMAIL_PASSWORD'],
+    :authentication => 'login'
+}
 end
