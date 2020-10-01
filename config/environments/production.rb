a1881b252e639b9522a4ed42a772383799c8082b
kento higa <kento_higa_0401@yahoo.co.jp>
2020-10-01 14:40:17 +0900

    edit home page view

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,22 +112,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'vast-ravine-63102.herokuapp.com' }
 
  config.action_mailer.delivery_method = :smtp
-
  config.action_mailer.smtp_settings = {
-
    :user_name => ENV['SENDGRID_USERNAME'],
-
    :password => ENV['SENDGRID_PASSWORD'],
-
    :domain => "heroku.com",
-
    :address => "smtp.sendgrid.net",
-
    :port => 587,
-
    :authentication => :plain,
-
    :enable_starttls_auto => true
-
  }
 end
